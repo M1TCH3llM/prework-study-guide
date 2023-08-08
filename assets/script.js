23a58c0aef991c0c736dcfab38a2730c6a3ddac9
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-08-08 13:35:38 -0600

    finished adding javascript functions

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,31 @@
-
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
 
-for(var x = 0; x < topics.length; x++) {
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-    console.log(topics[x]);
-
+function listToptics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x])
     }
-    
-/* 
-var topic = "HTML";
+}
 
-
-if (topic === 'HTML') {
+function selectTopic() {
+if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
     console.log("Let's study JavaScript!");
 } else {
     console.log("Please try again!");
+    }
 }
-*/
+
+console.log('here are the topics we learned through Prework:');
+
+listToptics()
+
+console.log('Whirch topic should we3 studay first?');
+
+selectTopic()
